8e10d96877cdb3453cc8b1f6c3fc9ed0ed937857
Daniel Waldorf <daniel@woodvillage.de>
2025-05-19 00:21:02 +0200

    [impl] switch to primieng and tailwind css

--- CloudBoard.Angular/src/app/app.component.ts
+++ CloudBoard.Angular/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- CloudBoard.Angular/src/app/controls/toolbar/toolbar.component.ts
+++ CloudBoard.Angular/src/app/controls/toolbar/toolbar.component.ts
@@ -1,80 +1,113 @@
-import { Component, inject, OnInit, TemplateRef } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild, viewChild } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { BoardProviderService } from '../../services/board-provider.service';
-import { NgbPopover, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbPopover, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FlowControlService } from '../../services/flow-control.service';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { PopoverModule } from 'primeng/popover';
 
 @Component({
   selector: 'toolbar',
-  imports: [NgClass, NgbPopover],
+  imports: [NgClass, MenuModule, PopoverModule/*NgbPopover*/],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent implements OnInit {
 
-  modalService: NgbModal = inject(NgbModal);
+  @ViewChild('menuBar') menuBar: any;
+  popover: any = viewChild('popover');
+
+  //modalService: NgbModal = inject(NgbModal);
   flowControlService: FlowControlService = inject(FlowControlService);
 
-  buttons: { icon: string; tooltip: string; action: () => void, popover: boolean | undefined }[] = [];
-  devBoardId: string = '126a505a-512b-48a0-99bc-84d02a86d7e7'; // Static GUID
+  menuItems: MenuItem[] = [];
   availableBoards: any[] = [];
   deletionBoard: any;
 
   boardProviderService: BoardProviderService = inject(BoardProviderService);
-  
-  constructor() {}
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.buttons.push({
-        icon: 'file-earmark-plus',
-        tooltip: 'Create',
-        action: () => this.onCreate(),
-        popover: false
-      });
-    this.buttons.push({
-        icon: 'folder',
-        tooltip: 'Open',
-        action: () => this.refreshAvailableBoards(),
-        popover: true
-      });
-    this.buttons.push({
-        icon: 'floppy',
-        tooltip: 'Save',
-        action: () => this.onSave(),
-        popover: false
-      });
-    this.buttons.push({
-        icon: 'separator',
-        tooltip: '',
-        action: () => {},
-        popover: false
-      });
-    this.buttons.push({
-        icon: 'zoom-in',
-        tooltip: 'Zoom In',
-        action: () => { this.flowControlService.zoomIn(); },
-        popover: false
-      });
-    this.buttons.push({
-        icon: 'zoom-out',
-        tooltip: 'Zoom Out',
-        action: () => { this.flowControlService.zoomOut(); },
-        popover: false
-      });
-    this.buttons.push({
-        icon: 'arrows-collapse-vertical',
-        tooltip: 'Reset Zoom',
-        action: () => { this.flowControlService.resetZoom(); },
-        popover: false
-      });
+    this.menuItems = [
+      {
+        label: 'Cloudboard',
+        items: [
+          {
+            label: 'Create',
+            icon: 'pi pi-fw pi-plus',
+            command: () => this.onCreate()
+          },
+          {
+            label: 'Open',
+            icon: 'pi pi-fw pi-download',
+            command: () => this.onOpen()
+          },
+          {
+            label: 'Save',
+            icon: 'pi pi-fw pi-save',
+            command: () => this.onSave()
+          }
+        ]
+      },
+      {
+        label: 'Viewport',
+        items: [
+          {
+            label: 'Reset',
+            icon: 'pi pi-fw pi-times-circle',
+            command: () => this.flowControlService.resetZoom()
+          },
+          {
+            label: 'Zoom In',
+            icon: 'pi pi-fw pi-search-plus',
+            command: () => this.flowControlService.zoomIn()
+          },
+          {
+            label: 'Zoom Out',
+            icon: 'pi pi-fw pi-search-minus',
+            command: () => this.flowControlService.zoomOut()
+          }
+        ]
+      }
+    ];
   }
 
   onCreate(): void {
     this.boardProviderService.createNewCloudBoard().subscribe();
   }
 
-  onOpen(boardId: string): void {
-    this.boardProviderService.loadCloudBoardById(boardId).subscribe();
+  onOpen(): void {
+    //this.boardProviderService.loadCloudBoardById(boardId).subscribe();
+    this.boardProviderService.listCloudBoards().subscribe(
+      response => {
+        this.availableBoards = response;
+
+        // Update the Open menu item with the latest available boards
+        const cloudboardMenu = this.menuItems[0]; // Get the Cloudboard menu
+        const openMenuItem = cloudboardMenu.items?.find(item => item.label === 'Open');
+
+        if (openMenuItem) {
+          openMenuItem.items = this.availableBoards.map(board => ({
+            label: board.name,
+            icon: 'pi pi-fw pi-file',
+            command: () => {
+              this.boardProviderService.loadCloudBoardById(board.id).subscribe(
+                () => {
+                },
+                error => {
+                  console.error('Error loading cloudboard', error);
+                }
+              );
+            }
+          }));
+          this.menuBar.visible = false;
+        }
+      },
+      error => {
+        console.error('Error fetching cloudboards', error);
+      });
   }
 
   onSave(): void {
@@ -83,22 +116,11 @@ export class ToolbarComponent implements OnInit {
 
   onDelete(boardId: string, content: TemplateRef<any>): void {
     this.deletionBoard = this.availableBoards.find(board => board.id === boardId);
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title',  }).result.then((result) => {
-      if (result === 'ok') {
-        this.boardProviderService.deleteCloudBoard(boardId).subscribe();
-        this.availableBoards = this.availableBoards.filter(board => board.id !== boardId);
-      }
-    });
-  }
-
-  refreshAvailableBoards(): void {
-    this.availableBoards = [];
-    this.boardProviderService.listCloudBoards().subscribe(
-      response => {
-        this.availableBoards = response;
-      },
-      error => {
-        console.error('Error fetching cloudboards', error);
-      });
+    // this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title',  }).result.then((result) => {
+    //   if (result === 'ok') {
+    //     this.boardProviderService.deleteCloudBoard(boardId).subscribe();
+    //     this.availableBoards = this.availableBoards.filter(board => board.id !== boardId);
+    //   }
+    // });
   }
 }
