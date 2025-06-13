fe649cc93f6ae1ad6dde86b2c458e2ead876803b
Daniel Waldorf <daniel@woodvillage.de>
2025-06-13 07:40:36 +0200

    [impl] add a home component

--- CloudBoard.Angular/src/app/app.routes.ts
+++ CloudBoard.Angular/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { FlowboardComponent } from './flowboard/flowboard.component'; // Adjust the path as needed
 import { ProjectsComponent } from './projects/projects.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'flowboard', component: FlowboardComponent },
   { path: 'flowboard/:id', component: FlowboardComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'timeline', component: TimelineComponent },
-  { path: '', redirectTo: 'flowboard', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
