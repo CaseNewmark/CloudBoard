3c46a7e637bd710976cd4214f1ba865b0d09bf6a
Daniel Waldorf <daniel@woodvillage.de>
2025-08-06 00:12:26 +0200

    [stop] state unknown

--- CloudBoard.AppHost/Program.cs
+++ CloudBoard.AppHost/Program.cs
@@ -2,25 +2,29 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder.AddKeycloak("keycloak", 8080)
                       .WithRealmImport("./Realms/cloudboard.json")
-                      .WithDataVolume();
+                      .WithDataVolume()
+                      .PublishAsContainer();
 
 var dbserver = builder.AddPostgres("postgres")
                       .WithDataVolume()
-                      .WithPgAdmin();
+                      .PublishAsContainer();
+                      //.WithPgAdmin();
 
 var database = dbserver.AddDatabase("cloudboard");
 
 var apiService = builder.AddProject<Projects.CloudBoard_ApiService>("apiservice")
-                        .WithReference(database)
-                        .WithReference(keycloak)
-                        .WaitFor(database)
-                        .WaitFor(keycloak)
-                        .WithHttpsHealthCheck("/health");
+//    .WithReference(database)
+//    .WithReference(keycloak)
+//    .WaitFor(database)
+//    .WaitFor(keycloak)
+//    .WithHttpsHealthCheck("/health");
+;
 
 builder.AddNpmApp("angular", "../CloudBoard.Angular")
-       .WithReference(keycloak)
-       .WithReference(apiService)
-       .WaitFor(apiService)
-       .WithExternalHttpEndpoints();
+       // .WithReference(keycloak)
+       // .WithReference(apiService)
+       // .WaitFor(apiService)
+       .WithExternalHttpEndpoints()
+       .PublishAsDockerFile();
 
 builder.Build().Run();
